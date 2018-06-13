c8d2cf93b3731bd5c596a8092c995c8e2d5c2fd9
riccardo caruso <riccardo135@gmail.com>
2018-06-13 09:52:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ var greeting = "Hello, everybody!";
 
 
 var specialGuest = "Neil deGrasse Tyson"
-"Hello, " + specialGuest + "!"
+
+var greetSpecialGuest = "Hello," + specialGuest + "!";
+
 var topic = "Let's talk about space";
 
-var conversation = `${talk}`;
+var conversation = `${topic}`;
